0a6e49d2472bdc7946447721693498894709a0e5
Crowdbotics <team@crowdbotics.com>
2023-03-30 19:24:20 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "clock-in-work-hours-39624",
+  url: "https://clock-in-work-hours-39624.botics.co",
+  api: "https://clock-in-work-hours-39624.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
